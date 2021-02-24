5336abd315b67290af88bb918b78227acd510554
ragsav <45696355+ragsav@users.noreply.github.com>
2021-02-24 10:59:29 +0530

    new host

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
   // HOST: "http://localhost:3000/",
-  HOST: "https://copybin-5de5c.web.app/",
+  HOST: "https://codebin4u.web.app/",
+  // HOST: "https://copybin-5de5c.web.app/",
   SERVERHOST: "https://copybinback.herokuapp.com",
 
   WEBSITE: "Code>Bin",
