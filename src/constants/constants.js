cd959378dd7dbf758d035bb4b0623f794f7f1d24
ragsav <45696355+ragsav@users.noreply.github.com>
2021-03-06 23:10:47 +0530

    repair

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 const Constants = Object.freeze({
-  HOST: "http://localhost:3000/",
-  // HOST: "https://codebin4u.web.app/",
+  // HOST: "http://localhost:3000/",
+  HOST: "https://codebin4u.web.app/",
   // HOST: "https://copybin-5de5c.web.app/",
   SERVERHOST: "https://copybinback.herokuapp.com",
 
@@ -12,7 +12,7 @@ const Constants = Object.freeze({
   ABOUT: "About",
 
   NUMBERTHEORY: "Number>Theory",
-  MOREPRODUCTS: "More>Utilities",
+  MOREPRODUCTS: "More utilities",
   DEFAULTTHEME: "monokai",
   DEFAULTMODE: "javascript",
 
